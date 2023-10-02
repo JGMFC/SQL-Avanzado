5e93369e29e1bb87882dcc569f29ec40fd2a9a6d
JGMFC <132767920+JGMFC@users.noreply.github.com>
2023-10-02 19:24:53 +0200

    Update CREATE OR REPLACE TABLE keepcoding.ivr_summary.sql

--- CREATE OR REPLACE TABLE keepcoding.ivr_summary.sql
+++ CREATE OR REPLACE TABLE keepcoding.ivr_summary.sql
@@ -26,26 +26,6 @@ WITH
         , LAG(start_date) OVER (PARTITION BY phone_number ORDER BY start_date) AS primera_llamada
         , LEAD(start_date) OVER (PARTITION BY phone_number ORDER BY start_date) AS segunda_llamada
       FROM keepcoding.ivr_calls)
-  , 
-  info_by_phone
-  AS (SELECT 
-          calls_ivr_id
-        , count(*) AS info_by_phone
-      FROM keepcoding.ivr_detail
-      WHERE 
-            step_name = "CUSTOMERINFOBYPHONE.TX"
-        AND step_description_error = "NULL"
-      GROUP BY calls_ivr_id,step_name,step_description_error)
-  ,
-  info_by_dni
-  AS (SELECT
-          calls_ivr_id
-        , count(*) AS info_by_dni
-      FROM keepcoding.ivr_detail
-      WHERE 
-            step_name = "CUSTOMERINFOBYDNI.TX"
-        AND step_description_error = "NULL"
-      GROUP BY calls_ivr_id,step_name,step_description_error)
 
 SELECT 
     detail.calls_ivr_id
@@ -68,8 +48,8 @@ SELECT
   , documents.customer_phone
   , documents.billing_account
   , IF(CONTAINS_SUBSTR(calls_module_aggregation, "AVERIA_MASIVA"), 1, 0) AS masiva_lg
-  , COALESCE(info_by_phone.info_by_phone, 0) as info_by_phone_lg
-  , COALESCE(info_by_dni.info_by_dni, 0) as  info_by_dni_lg
+  , CASE WHEN SUM(IF((detail.step_name = 'CUSTOMERINFOBYPHONE.TX') AND (detail.step_description_error = 'NULL' ), 1, 0)) > 0 THEN 1 ELSE 0 END AS info_by_phone_lg
+  , CASE WHEN SUM(IF((detail.step_name = 'CUSTOMERINFOBYDNI.TX') AND (detail.step_description_error = 'NULL' ), 1, 0)) > 0 THEN 1 ELSE 0 END AS info_by_dni_lg
   , IF(DATETIME_DIFF(detail.calls_start_date, llamadas.primera_llamada,HOUR)<24,1,0) AS repeated_phone_24H
   , IF(DATETIME_DIFF(llamadas.segunda_llamada,detail.calls_end_date,HOUR)<24,1,0) AS cause_recall_phone_24H
 FROM keepcoding.ivr_detail detail
@@ -82,14 +62,22 @@ LEFT
 LEFT
   JOIN documents
   ON detail.calls_ivr_id = documents.ivr_id
-LEFT 
-  JOIN info_by_phone
-  ON info_by_phone.calls_ivr_id = detail.calls_ivr_id
-LEFT 
-  JOIN info_by_dni
-  ON info_by_dni.calls_ivr_id = detail.calls_ivr_id
-QUALIFY ROW_NUMBER() 
-  OVER(
-    PARTITION BY CAST(detail.calls_ivr_id AS STRING) 
-    ORDER BY detail.calls_ivr_id,detail.calls_start_date DESC
-  ) = 1
+  GROUP BY
+    detail.calls_ivr_id
+  , calls_phone_number
+  , calls_ivr_result
+  , calls_vdn_aggregation
+  , calls_start_date
+  , calls_end_date
+  , calls_total_duration
+  , calls_customer_segment
+  , calls_ivr_language
+  , calls_steps_module
+  , calls_module_aggregation
+  , documents.document_type
+  , documents.document_identification
+  , documents.customer_phone
+  , documents.billing_account
+  , llamadas.primera_llamada
+  , llamadas.segunda_llamada;
+
